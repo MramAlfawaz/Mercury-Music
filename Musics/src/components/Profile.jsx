64a7885d9bb6c3ad2338fc07ff9155ecb2d03a56
Bashayr Alhasani <bashaier.alhassani@gmail.com>
2020-04-21 20:26:41 +0300

    User Profile Added

--- Musics/src/components/Profile.jsx
+++ Musics/src/components/Profile.jsx
@@ -3,17 +3,14 @@ import FavoritesAlbums from "./FavoritesAlbums";
 
 export default class Profile extends Component {
   render() {
-      console.log(this.props)
+    console.log(this.props);
     let user = this.props.user;
     return (
       <div>
-        {/* <img src={user.image}></img> */}
         <h2>{user.firstName}</h2>
-        <h3> last name : {user.lastName} </h3>
-        <FavoritesAlbums/>
-
+        <h3> {user.lastName} </h3>
+        <FavoritesAlbums />
       </div>
-
     );
   }
 }
